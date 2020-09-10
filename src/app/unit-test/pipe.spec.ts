65a6dfa3efa40ac98ccab40bb55a0a4e48f1d542
ConteAlexandre <pro.alexandre.conte@gmail.com>
2020-09-10 15:18:56 +0200

    Update

--- src/app/unit-test/pipe.spec.ts
+++ src/app/unit-test/pipe.spec.ts
@@ -12,4 +12,23 @@ describe('phonenumberpipe', () => {
         const res = phoneNumber.transform(telToTest);
         expect(res).toBe(expectation);
     });
+
+    it('less than digit option', function () {
+        const telToTest = '097865321';
+        const expectation = '';
+        const res = phoneNumber.transform(telToTest);
+        expect(res).toBe(expectation);
+    });
+    it('dots option', function () {
+        const telToTest = '0978654321';
+        const expectation = '097.865.4321';
+        const res = phoneNumber.transform(telToTest, 'dots');
+        expect(res).toBe(expectation);
+    });
+    it('hyphens option', function () {
+        const telToTest = '0978654321';
+        const expectation = '097-865-4321';
+        const res = phoneNumber.transform(telToTest, 'hyphens');
+        expect(res).toBe(expectation);
+    });
 })
